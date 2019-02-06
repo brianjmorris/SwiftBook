8782623f00ad20bb100f41afed7b8dcc24dceffd
Brian Morris <contact@brianmorris.info>
2019-02-06 18:08:45 -0500

    DB Connection

---
+++ WebAPI/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>COSC3506 Project</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Raleway" rel="stylesheet">
+
+    <style>
+      body, html {
+        font-size: 18px;
+        font-family: 'Noto Sans', sans-serif;
+        background-color: #111111;
+        color: #ececec;
+      }
+
+      h1, h2, h3, h4, h5, h6 {
+        font-family: 'Raleway', sans-serif;
+      }
+
+      img[src*="000webhost"],
+      img[alt*="000webhost"],
+      a[title*="000webhost"],
+      a[href*="000webhost"] {
+        display:none
+      }
+    </style>
+
+    <!-- connect to database -->
+    <?php include("db.php"); ?>
+
+  </head>
+
+  <body>
+
+    <h1>Software Engineering Project</h1>
+    <p>This is a temporary host for our project's backend, the Web API</p>
+
+  </body>
+</html>
