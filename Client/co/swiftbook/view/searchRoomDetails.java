95a43d6980b3b3360ed87db696dae7985587cf2e
Amanda <anderson_amanda@mail.com>
2019-03-31 21:13:48 -0400

    SearchRoomDetails Updates

--- Client/co/swiftbook/view/searchRoomDetails.java
+++ Client/co/swiftbook/view/searchRoomDetails.java
@@ -7,6 +7,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -28,7 +30,7 @@ public class searchRoomDetails extends Application {
     	// View title
         primaryStage.setTitle("SwiftBook");
         BorderPane root = new BorderPane();
-        root.setPadding(new Insets(170, 0, 115, 0));
+        root.setPadding(new Insets(170, 200, 115, 200));
         Scene scene = new Scene(root, 1000, 800);
         scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
         
@@ -38,23 +40,33 @@ public class searchRoomDetails extends Application {
         imageView.setImage(softwareName);
         Text dashTitle = new Text("Search Meeting Details");
         VBox viewTitle = new VBox();
-        
+        viewTitle.requestFocus();
+
         // User Options
         VBox details = new VBox();
-        
+       
+        TextField roomID = new TextField();
+        TextArea roomDetails = new TextArea();
         Button search = new Button("Search");
         
         viewTitle.getChildren().addAll(imageView, dashTitle);
-        details.getChildren().addAll(search);
+        details.getChildren().addAll(roomID, roomDetails, search);
 
         // Styling
         viewTitle.getStyleClass().add("vbox");
         viewTitle.setAlignment(Pos.CENTER);
         details.getStyleClass().add("vbox");
         details.setAlignment(Pos.CENTER);
+        details.setPadding(new Insets(40, 0, 40, 0));
+        details.setSpacing(60);
         dashTitle.getStyleClass().add("dashTitle");
         search.getStyleClass().add("dashButton");
-
+        roomID.getStyleClass().add("form");
+        roomID.setPromptText("Room ID");
+        roomDetails.getStyleClass().add("form");
+        roomDetails.setMaxHeight(200);
+        roomDetails.setPromptText("Click Search to Display Room Details");
+        
         // Add To View
         root.setTop(viewTitle);
         root.setBottom(details);
